2b5757cdfa4a5656b370d7bc5f8b4f07c7a673f9
R4R35 <rockforthewinxd@gmail.com>
2026-03-30 15:32:01 +0300

    Added MongoDB indexes on telemetry_records collection

--- src/main/java/p2ps/telemetry/model/TelemetryRecord.java
+++ src/main/java/p2ps/telemetry/model/TelemetryRecord.java
@@ -2,21 +2,32 @@ package p2ps.telemetry.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "telemetry_records")
+@CompoundIndex(def = "{'storeId': 1, 'itemId': 1}")
 public class TelemetryRecord {
 
     @Id
     private String id;
 
     private String deviceId;
+
+    @Indexed
     private String storeId;
+
+    @Indexed
     private String itemId;
+
     private Double lat;
     private Double lng;
     private Double accuracyMeters;
+
     private Long timestamp;
+
+    @Indexed(expireAfter = "P3Y") //expires after 3 yeards
     private Long serverReceivedTimestamp;
 }
